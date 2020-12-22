48ae3af374e4d1184390e1af2bf7920ba50d8773
SumanDontha <dontha.suman@gmail.com>
2020-12-22 11:45:31 +0000

    Added the info section.

---
+++ real-estate-app/src/data/InfoData.js
@@ -0,0 +1,26 @@
+import ImageOne from "../images/interior-1.jpg";
+import ImageTwo from "../images/interior-2.jpg";
+
+export const InfoData = {
+  heading: "Explore our beautiful homes.",
+  paragraphOne:
+    "Lorem Cupidatat velit labore ullamco sunt sint non. Nostrud et dolore laboris labore velit est pariatur dolor nulla. Elit dolore quis mollit aute ex duis veniam mollit elit in consequat minim. Nostrud irure in cupidatat laborum. Excepteur tempor minim est adipisicing elit in eu culpa.",
+  paragraphTwo:
+    "Lorem Cupidatat velit labore ullamco sunt sint non. Nostrud et dolore laboris labore velit est pariatur dolor nulla. Elit dolore quis mollit aute ex duis veniam mollit elit in consequat minim. Nostrud irure in cupidatat laborum. Excepteur tempor minim est adipisicing elit in eu culpa.",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Modern Designs.",
+  paragraphOne:
+    "Nostrud et dolore laboris labore velit est pariatur dolor nulla. Elit dolore quis mollit aute ex duis veniam mollit elit in consequat minim. Nostrud irure in cupidatat laborum. Excepteur tempor minim est adipisicing elit in eu culpa.",
+  paragraphTwo:
+    "Cupidatat velit labore ullamco sunt sint non. Nostrud et dolore laboris labore velit est pariatur dolor nulla. Elit dolore quis mollit aute ex duis veniam mollit elit in consequat minim.",
+  buttonLabel: "View Homes",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
